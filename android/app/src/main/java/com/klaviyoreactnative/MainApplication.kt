6816d474d00a7066801a6eae0f5557b45ce1565f
Ajay Subramanya <ajay.subramanya@klaviyo.com>
2023-09-28 22:00:22 -0500

    working android bridging

--- android/app/src/main/java/com/klaviyoreactnative/MainApplication.kt
+++ android/app/src/main/java/com/klaviyoreactnative/MainApplication.kt
@@ -5,18 +5,9 @@ import com.facebook.react.BuildConfig
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this,  /* native exopackage */false)
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    }
-
     override fun getReactNativeHost() = object : ReactNativeHost(this) {
         override fun getUseDeveloperSupport() = BuildConfig.DEBUG
 
@@ -25,5 +16,11 @@ class MainApplication : Application(), ReactApplication {
         }
 
         override fun getJSMainModuleName() = "index"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this,  /* native exopackage */false)
+//        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
 }
