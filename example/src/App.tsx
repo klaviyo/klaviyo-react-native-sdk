c35fc7dceb7934afa9dab6e8e13cc91d354d9325
kennyklaviyo <kenny.tsui@klaviyo.com>
2023-12-21 11:57:06 -0500

    fix merge

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,36 +1,15 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text, Button } from 'react-native';
-<<<<<<< HEAD
 import { Klaviyo, EventProperty, EventType } from 'klaviyo-react-native-sdk';
-=======
-import {
-  multiply,
-  Klaviyo,
-  KlaviyoEventName,
-  KlaviyoEventProperty
-} from 'klaviyo-react-native-sdk';
->>>>>>> 2426f1f (create event interface)
 
 export default function App() {
   const [result] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-<<<<<<< HEAD
-    Klaviyo.createEvent({
-      event: EventType.CUSTOM('EXAMPLE_EVENT'),
-      properties: {
-        [EventProperty.EVENT_ID.name]: '123',
-=======
-    multiply(3, 7).then(setResult);
-    Klaviyo.createEvent(
-      KlaviyoEventName.OPENED_PUSH,
-      {
-        "custom_property": '123',
-        [KlaviyoEventProperty.VALUE]: 1,
->>>>>>> 2426f1f (create event interface)
-      },
-    );
+    Klaviyo.createEvent(EventType.OPENED_PUSH, {
+      [EventProperty.EVENT_ID]: '123',
+    });
   }, []);
 
   return (
@@ -39,13 +18,10 @@ export default function App() {
       <Button
         title="Click Me"
         onPress={() => {
-          Klaviyo.createCustomEvent(
-            "CUSTOM_EVENT",
-            {
-              "custom_property_2": '321',
-              [KlaviyoEventProperty.VALUE]: 12,
-            },
-          );
+          Klaviyo.createCustomEvent('CUSTOM_EVENT', {
+            custom_property_2: '321',
+            [EventProperty.VALUE]: 12,
+          });
         }}
       />
     </View>
