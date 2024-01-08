a9d6072c55674e5bc2a5e60c75760bf18115c747
Evan Masseau <>
2024-01-08 11:35:00 -0500

    Lint after rebasing

--- android/src/main/java/com/klaviyoreactnativesdk/KlaviyoReactNativeSdkModule.kt
+++ android/src/main/java/com/klaviyoreactnativesdk/KlaviyoReactNativeSdkModule.kt
@@ -16,109 +16,116 @@ import kotlin.reflect.KVisibility
 
 class KlaviyoReactNativeSdkModule internal constructor(private val context: ReactApplicationContext) :
   KlaviyoReactNativeSdkSpec(context) {
-
-  companion object {
-    const val NAME = "KlaviyoReactNativeSdk"
-    private const val LOCATION = "location"
-    private const val PROPERTIES = "properties"
-  }
+    companion object {
+      const val NAME = "KlaviyoReactNativeSdk"
+      private const val LOCATION = "location"
+      private const val PROPERTIES = "properties"
+    }
 
     override fun getName(): String {
       return NAME
     }
 
-  override fun getConstants(): MutableMap<String, Any> {
-    return hashMapOf(
-      "PROFILE_KEYS" to this.extractConstants<ProfileKey>().toMutableMap().apply {
-        this[LOCATION] = LOCATION
-        this[PROPERTIES] = PROPERTIES
-      },
-      "EVENT_NAMES" to this.extractConstants<EventType>(),
-    )
-  }
-
-  private inline fun <reified T> extractConstants(): Map<String, String> where T : Keyword {
-    return T::class.nestedClasses.filter {
-      it.visibility == KVisibility.PUBLIC && it.objectInstance != null
-    }.associate {
-      it.simpleName.toString() to (it.objectInstance as T).name
+    override fun getConstants(): MutableMap<String, Any> {
+      return hashMapOf(
+        "PROFILE_KEYS" to
+          this.extractConstants<ProfileKey>().toMutableMap().apply {
+            this[LOCATION] = LOCATION
+            this[PROPERTIES] = PROPERTIES
+          },
+        "EVENT_NAMES" to this.extractConstants<EventType>(),
+      )
     }
-  }
 
-  @ReactMethod
-  override fun initialize(apiKey: String) {
-    Klaviyo.initialize(apiKey, context)
-  }
-
-  @ReactMethod
-  override fun setProfile(profile: ReadableMap) {
-    val parsedProfile = Profile()
-
-    profile.toHashMap().forEach { (key, value) ->
-      when (key) {
-        LOCATION, PROPERTIES -> (value as? HashMap<*, *>)?.forEach { (key, value) ->
-          if (key is String && value is Serializable) {
-            parsedProfile[key] = value
-          }
-        }
-        else -> if (value is Serializable) {
-          parsedProfile[key] = value
-        }
+    private inline fun <reified T> extractConstants(): Map<String, String> where T : Keyword {
+      return T::class.nestedClasses.filter {
+        it.visibility == KVisibility.PUBLIC && it.objectInstance != null
+      }.associate {
+        it.simpleName.toString() to (it.objectInstance as T).name
       }
     }
 
-    Klaviyo.setProfile(parsedProfile)
-  }
+    @ReactMethod
+    override fun initialize(apiKey: String) {
+      Klaviyo.initialize(apiKey, context)
+    }
 
-  @ReactMethod
-  override fun setExternalId(externalId: String) {
-    Klaviyo.setExternalId(externalId)
-  }
+    @ReactMethod
+    override fun setProfile(profile: ReadableMap) {
+      val parsedProfile = Profile()
 
-  @ReactMethod
-  override fun getExternalId(callback: Callback) {
-    callback.invoke(Klaviyo.getExternalId())
-  }
+      profile.toHashMap().forEach { (key, value) ->
+        when (key) {
+          LOCATION, PROPERTIES ->
+            (value as? HashMap<*, *>)?.forEach { (key, value) ->
+              if (key is String && value is Serializable) {
+                parsedProfile[key] = value
+              }
+            }
+          else ->
+            if (value is Serializable) {
+              parsedProfile[key] = value
+            }
+        }
+      }
 
-  @ReactMethod
-  override fun setEmail(email: String) {
-    Klaviyo.setEmail(email)
-  }
+      Klaviyo.setProfile(parsedProfile)
+    }
 
-  @ReactMethod
-  override fun getEmail(callback: Callback) {
-    callback.invoke(Klaviyo.getEmail())
-  }
+    @ReactMethod
+    override fun setExternalId(externalId: String) {
+      Klaviyo.setExternalId(externalId)
+    }
 
-  @ReactMethod
-  override fun setPhoneNumber(phoneNumber: String) {
-    Klaviyo.setPhoneNumber(phoneNumber)
-  }
+    @ReactMethod
+    override fun getExternalId(callback: Callback) {
+      callback.invoke(Klaviyo.getExternalId())
+    }
 
-  @ReactMethod
-  override fun getPhoneNumber(callback: Callback) {
-    callback.invoke(Klaviyo.getPhoneNumber())
-  }
+    @ReactMethod
+    override fun setEmail(email: String) {
+      Klaviyo.setEmail(email)
+    }
 
-  @ReactMethod
-  override fun setProfileAttribute(propertyKey: String, value: String) {
-    Klaviyo.setProfileAttribute(ProfileKey.CUSTOM(propertyKey), value)
-  }
+    @ReactMethod
+    override fun getEmail(callback: Callback) {
+      callback.invoke(Klaviyo.getEmail())
+    }
 
-  @ReactMethod
-  override fun resetProfile() {
-    Klaviyo.resetProfile()
-  }
+    @ReactMethod
+    override fun setPhoneNumber(phoneNumber: String) {
+      Klaviyo.setPhoneNumber(phoneNumber)
+    }
 
-  @ReactMethod
-  override fun createEvent(event: ReadableMap) {
-    val event = Event(
-      type = event.getString("name")!!.let { EventType.CUSTOM(it) },
-      properties = event.getMap("properties")?.toHashMap()
-        ?.map { entry -> EventKey.CUSTOM(entry.key) as EventKey to entry.value as Serializable }
-        ?.toMap(),
-    )
-      .setProperty(EventKey.VALUE, event.getDouble("value"))
-    Klaviyo.createEvent(event = event)
+    @ReactMethod
+    override fun getPhoneNumber(callback: Callback) {
+      callback.invoke(Klaviyo.getPhoneNumber())
+    }
+
+    @ReactMethod
+    override fun setProfileAttribute(
+      propertyKey: String,
+      value: String,
+    ) {
+      Klaviyo.setProfileAttribute(ProfileKey.CUSTOM(propertyKey), value)
+    }
+
+    @ReactMethod
+    override fun resetProfile() {
+      Klaviyo.resetProfile()
+    }
+
+    @ReactMethod
+    override fun createEvent(event: ReadableMap) {
+      val event =
+        Event(
+          type = event.getString("name")!!.let { EventType.CUSTOM(it) },
+          properties =
+            event.getMap("properties")?.toHashMap()
+              ?.map { entry -> EventKey.CUSTOM(entry.key) as EventKey to entry.value as Serializable }
+              ?.toMap(),
+        )
+          .setProperty(EventKey.VALUE, event.getDouble("value"))
+      Klaviyo.createEvent(event = event)
+    }
   }
-}
