843254ffd00424fd15ad8ce2fe33bdf8868d2087
Ajay Subramanya <118314354+ajaysubra@users.noreply.github.com>
2024-01-24 14:30:09 -0600

    Using major versions for native platform SDKs & coming out of alpha for RN SDK (#47)
    * chnl-4781: iOS readme updates to intilization steps
    
    * fixed typo
    
    * chnl-4782: beta dev state update
    
    * chnl-4783: updated events
    
    * some more readme updates
    
    * Using major versions in RN SDK
    
    * adding podfile lock and initilize for android
    
    * registering lifecycle events for andrioid

--- example/android/app/src/main/java/com/klaviyoreactnativesdkexample/MainApplication.kt
+++ example/android/app/src/main/java/com/klaviyoreactnativesdkexample/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.klaviyo.analytics.Klaviyo
 
 class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost =
@@ -35,6 +36,9 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
+    Klaviyo.initialize("YOUR_PUBLIC_API_KEY", this)
+    registerActivityLifecycleCallbacks(Klaviyo.lifecycleCallbacks)
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
