4c7ffa5f6932710b3097a111ca9e694a86975878
Ajay Subramanya <ajay.subramanya@klaviyo.com>
2024-02-09 11:46:03 -0600

    udpate the example app to implement push

--- example/ios/KlaviyoReactNativeSdkExample/PushNotificationsHelper.swift
+++ example/ios/KlaviyoReactNativeSdkExample/PushNotificationsHelper.swift
@@ -4,35 +4,39 @@ import UserNotifications
 import KlaviyoSwift
 
 @objc(PushNotificationsHelper)
+/// Helper class to act as a swift layer between react native's objective c code and Klaviyo SDK's swift code
 class PushNotificationsHelper: NSObject {
   @objc
-  static func initializeSDK() {
-    KlaviyoSDK().initialize(with: "YOUR_PUBLIC_API_KEY")
+  static func initializeSDK(_ apiKey: String) {
+    // Installation Step 4: Set your company's public api key here
+    KlaviyoSDK().initialize(with: apiKey)
   }
 
   @objc
   static func requestPushPermission() {
-    // since we need the SDK to be initialized before setting the push token to it
-    initializeSDK()
     let center = UNUserNotificationCenter.current()
     center.requestAuthorization(options: [.alert, .sound, .badge]) { _, error in
         if let error = error {
           print("AuthError", "error while trying to authorize push", error)
         } else {
-          self.registerForRemoteNotifications()
+          // Installation Step 5: register for remote notifications after requesting permission on the main thread 
+          DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+          }
         }
-    }
-  }
-
-  @objc
-  static func registerForRemoteNotifications() {
-    DispatchQueue.main.async {
-      UIApplication.shared.registerForRemoteNotifications()
     }
   }
 
   @objc
   static func setPushToken(token: Data) {
     KlaviyoSDK().set(pushToken: token)
+  }
+
+  @objc
+  static func handleReceivingPush(response: UNNotificationResponse, completionHandler: @escaping () -> Void ) {
+    let handled = KlaviyoSDK().handle(notificationResponse: response, withCompletionHandler: completionHandler)
+    if !handled {
+      completionHandler()
+    }
   }
 }
