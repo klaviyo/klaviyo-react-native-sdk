99e209980f43a5fe9266247dd0fdf7afa6d36014
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-03-17 15:53:11 -0400

    add conditional import for header file

--- ios/KlaviyoFormsReactNativeSdk.mm
+++ ios/KlaviyoFormsReactNativeSdk.mm
@@ -1,8 +1,11 @@
 // KlaviyoFormsReactNativeSdk.m
 #import <React/RCTBridgeModule.h>
 #import "KlaviyoFormsReactNativeSdk-Bridging-Header.h"
+#if __has_include(<klaviyo_react_native_sdk/klaviyo_react_native_sdk-Swift.h>)
 #import <klaviyo_react_native_sdk/klaviyo_react_native_sdk-Swift.h>
-// TODO: add __has_include(<klaviyo_react_native_sdk/klaviyo_react_native_sdk-Swift.h>) conditional like KlaviyoReactNativeSdk.m
+#else
+#import "klaviyo_react_native_sdk-Swift.h"
+#endif
 
 @implementation KlaviyoFormsReactNativeSdk
 RCT_EXPORT_MODULE()
