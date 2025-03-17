8a506fc37001911ac4294435e4abf8b3bf234621
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-03-17 16:15:36 -0400

    use conditional import for KlaviyoForms as well

--- ios/KlaviyoFormsReactNativeSdk.mm
+++ ios/KlaviyoFormsReactNativeSdk.mm
@@ -1,6 +1,10 @@
 // KlaviyoFormsReactNativeSdk.m
 #import <React/RCTBridgeModule.h>
+#if __has_include(<klaviyo_react_native_sdk/KlaviyoFormsReactNativeSdk-Bridging-Header.h>)
+#import <klaviyo_react_native_sdk/KlaviyoFormsReactNativeSdk-Bridging-Header.h>
+#else
 #import "KlaviyoFormsReactNativeSdk-Bridging-Header.h"
+#endif
 #if __has_include(<klaviyo_react_native_sdk/klaviyo_react_native_sdk-Swift.h>)
 #import <klaviyo_react_native_sdk/klaviyo_react_native_sdk-Swift.h>
 #else
