13cac38872e348ac76f94ec1151b320b155a00a7
Evan C Masseau <5167687+evan-masseau@users.noreply.github.com>
2025-03-26 17:17:09 -0400

    Android: Explicitly set an initial value for currentActivity upon initializing via typescript. (#207)
    * Pass along the current activity from react SDK to android SDK.
    Relies on an Android PR that turned currentActivity into a var.
    
    * Use updated API for android activity override
    
    ---------
    
    Co-authored-by: Evan Masseau <>

--- android/src/main/java/com/klaviyoreactnativesdk/KlaviyoReactNativeSdkModule.kt
+++ android/src/main/java/com/klaviyoreactnativesdk/KlaviyoReactNativeSdkModule.kt
@@ -14,6 +14,7 @@ import com.klaviyo.analytics.model.Keyword
 import com.klaviyo.analytics.model.Profile
 import com.klaviyo.analytics.model.ProfileKey
 import com.klaviyo.core.Registry
+import com.klaviyo.core.utils.AdvancedAPI
 import com.klaviyo.forms.registerForInAppForms
 import java.io.Serializable
 import kotlin.reflect.KVisibility
@@ -49,7 +50,12 @@ class KlaviyoReactNativeSdkModule(
       }
 
   @ReactMethod
+  @OptIn(AdvancedAPI::class)
   fun initialize(apiKey: String) {
+    // Since initialize is being called after Application.onCreate,
+    // we must hand over a reference to the current activity.
+    // The native SDK will track Activity changes internally from here on.
+    currentActivity?.let(Registry.lifecycleMonitor::assignCurrentActivity)
     Klaviyo.initialize(apiKey, reactContext)
   }
 
