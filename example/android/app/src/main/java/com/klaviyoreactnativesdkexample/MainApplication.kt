da487201e45c5403d0803d69b669b8ebb31a85d2
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-08-13 14:48:11 -0400

    0.80.2 bump

--- example/android/app/src/main/java/com/klaviyoreactnativesdkexample/MainApplication.kt
+++ example/android/app/src/main/java/com/klaviyoreactnativesdkexample/MainApplication.kt
@@ -5,13 +5,11 @@ import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
 import com.google.firebase.messaging.FirebaseMessaging
 import com.klaviyo.analytics.Klaviyo
 
@@ -39,11 +37,7 @@ class MainApplication :
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative()
 
     if (BuildConfig.INITIALIZE_KLAVIYO_FROM_NATIVE) {
       // Android Installation Step 3: Initialize the SDK with public key and context, if initializing from native code
--- example/src/KlaviyoReactWrapper.ts
+++ example/src/KlaviyoReactWrapper.ts
@@ -21,7 +21,7 @@ export const initialize = async () => {
     // Alternate Android Installation Step 3
     // Alternate iOS Installation Step 3
     // Initialize the SDK with public key, if initializing from React Native
-    Klaviyo.initialize('YOUR_KLAVIYO_PUBLIC_API_KEY');
+    Klaviyo.initialize('XNhKEQ');
   } catch (e: any) {
     console.log(e.message, e.code);
   }
