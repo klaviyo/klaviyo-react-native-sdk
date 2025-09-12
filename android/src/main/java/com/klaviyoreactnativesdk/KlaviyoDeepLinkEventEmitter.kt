e862e8141e63c602424be1c371b17a3133f90b95
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-09-12 14:55:33 -0500

    wired up Android deep link handler

---
+++ android/src/main/java/com/klaviyoreactnativesdk/KlaviyoDeepLinkEventEmitter.kt
@@ -0,0 +1,44 @@
+package com.klaviyoreactnativesdk
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.klaviyo.analytics.Klaviyo
+
+class KlaviyoDeepLinkEventEmitter(
+  private val reactContext: ReactApplicationContext,
+) : com.facebook.react.bridge.ReactContextBaseJavaModule(reactContext) {
+  companion object {
+    const val NAME = "KlaviyoDeepLinkEventEmitter"
+    private const val DEEP_LINK_EVENT = "klaviyoDeepLink"
+    private var sharedInstance: KlaviyoDeepLinkEventEmitter? = null
+
+    fun getSharedInstance(): KlaviyoDeepLinkEventEmitter? = sharedInstance
+  }
+
+  init {
+    sharedInstance = this
+  }
+
+  override fun getName(): String = NAME
+
+  fun emitDeepLinkEvent(url: String) {
+    val params: WritableMap =
+      Arguments.createMap().apply {
+        putString("url", url)
+      }
+
+    reactContext
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      .emit(DEEP_LINK_EVENT, params)
+  }
+
+  @ReactMethod
+  fun registerDeepLinkHandler() {
+    Klaviyo.registerDeepLinkHandler { uri ->
+      emitDeepLinkEvent(uri.toString())
+    }
+  }
+}
