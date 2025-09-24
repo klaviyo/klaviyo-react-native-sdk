1836e6bdf99ce3020ab634fe062cad8c80f58e2e
Evan Masseau <>
2025-09-24 14:30:39 -0400

    oh dear github.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -24,7 +24,6 @@ export default function App() {
       console.log('Initial Url: url', url);
       handleUrl(url);
     });
-    });
 
     // Listen for deep link events now that the app is running
     Linking.addEventListener('url', ({ url }) => {
